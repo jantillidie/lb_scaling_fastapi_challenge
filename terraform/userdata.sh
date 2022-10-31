97fd7af878c2c425c1a27dcf4977ff19bdba76ad
Jan Maaß <git@antipost.de>
2022-10-31 13:01:29 +0100

    Fix userdata

--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -3,5 +3,5 @@ cd /home/ec2-user/
 wget https://jansfastapibucket87293847.s3.amazonaws.com/fastapi.zip
 unzip fastapi.zip
 rm fastapi.zip
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 python3 jobs.py
